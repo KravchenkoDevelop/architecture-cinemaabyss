e40c11eeb631f5bcf0e7cbef0d0e7650ed29e782
Tatiana <kravchenkodevelop@gmail.com>
2025-08-28 23:44:35 +0300

    it builds

--- src/microservices/events/events-service/Program.cs
+++ src/microservices/events/events-service/Program.cs
@@ -1,9 +1,24 @@
+using events_service;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// MVC
 builder.Services.AddControllers();
+
+// Swagger (для dotnet tool run swagger)
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Random event generator
 builder.Services.AddSingleton<RandomEventGenerator>();
 
 var app = builder.Build();
 
+// Swagger middleware (можно оставить для runtime)
+app.UseSwagger();
+app.UseSwaggerUI();
+
+// контроллеры
 app.MapControllers();
 
 // health check

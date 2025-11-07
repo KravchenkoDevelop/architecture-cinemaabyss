836cd7330eb816207db20d20e54311dcd454b1d2
Tatiana <kravchenkodevelop@gmail.com>
2025-11-08 00:45:06 +0300

    123

--- src/microservices/events/events-service/Events.API/Program.cs
+++ src/microservices/events/events-service/Events.API/Program.cs
@@ -1,24 +1,21 @@
-
 using Events.API;
+using Confluent.Kafka;
 
 var builder = WebApplication.CreateBuilder(args);
-var configuration = new ConfigurationBuilder()
-    .SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile("appsettings.json", optional: false)
-    .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true)
-    .AddEnvironmentVariables()
-    .Build();
-
-builder.Services.AddHostedService<ConsumerService>();
+var configuration = builder.Configuration;
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
+// Kafka producer
+builder.Services.AddSingleton<IProducer<string, string>>(_ =>
+{
+    var brokers = Environment.GetEnvironmentVariable("KAFKA_BROKERS") ?? "localhost:9092";
+    var config = new ProducerConfig { BootstrapServers = brokers };
+    return new ProducerBuilder<string, string>(config).Build();
+});
+
+
 var app = builder.Build();
-
-
-app.UseHttpsRedirection();
 app.MapControllers();
-
 app.Run();
